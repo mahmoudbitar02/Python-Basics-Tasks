c36ef913a811e6d2b16e89bae83cea7ec433b0c0
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-04 21:01:44 +0100

    add Variable tasks

---
+++ 01-Variables/tasks.py
@@ -0,0 +1,43 @@
+#1. Create a Boolean variable named x
+x = False
+
+#2. Create an integer variable named y
+y = 2
+
+#3. Create a float variable named z
+z = 2.15
+
+#4. Create a string variable names S
+s = "Mahmoud" or "10"
+
+#5. Convert the int variable to float
+f = 5/6
+
+#6. Can we convert the str to int ? -----------------
+yes,  int ("12")
+type (int('12'))
+
+#7. Create a list of numbers from 1 to 5
+e = ['1','2','3','4','5']
+
+#8. Create a tuple from 10 to 15
+r = ('10','11','12','13','14','15')
+
+#9. Convert the list to tuple ----------------------
+
+
+#10. Create a dict of 3 values
+
+m = {'Mahmoud':30 , 'Samer':60 , 'Hasan':90}
+
+#11. Can we use semi colon ; with python
+
+yes we can, BUT it no need to use it becouse python is a simple coding language
+if  we palce it we will not get an error
+
+#12. Puthon is interpreted or compiled ?
+python is interpreted
+
+#13. What is the differences between low level & high level
+
+Machine can understand tht low level esily in comparison of human. On ather hand humen or Programmers understand the high level easily in comparison of Machine
