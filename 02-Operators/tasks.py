327e02b52b33680b3a06c16d3f1a7700b8ee86be
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-04 23:20:21 +0100

    add operators

---
+++ 02-Operators/tasks.py
@@ -0,0 +1,21 @@
+#1.What is the differences between =,==
+'''
+    = to cereat a variable
+    == question( x==4) is x equel 4
+'''
+#2. What do we mean bu using !=
+'''
+    (y!= 3) asking if y not equel 3 
+'''
+#3. What is the operator precedence
+'''
+operator Precedence mean: example (4+4*2) (2+2*4)
+() we must arithmetic the each () than we should multiplication the two () ()
+(12)(10) = 120 
+
+'''
+#4. Create a variable × with value of 10
+'''
+x = 10
+'''
+#5. Increase × value by 15 using assignment operators
