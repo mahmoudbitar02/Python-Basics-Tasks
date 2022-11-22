8288bb2bf36d780647496d6e4902c5a82a703d66
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-22 21:32:54 +0100

    add answers

--- 06-Data Types/Data Types taskes.py
+++ 06-Data Types/Data Types taskes.py
@@ -13,8 +13,24 @@ lis=[10,11,12,13,14,15,16,17,18,19,20]
 lis.append(30)
 print(lis)
 
+#5. Add 30 to the end of the list
+lis=[10,11,12,13,14,15,16,17,18,19,20]
+lis.append(30)
+print(lis)
+
 #6. Add 40 as the second element of the list
 lis=[10,11,12,13,14,15,16,17,18,19,20]
-lis.append([1])
+lis.append(30)
+lis.insert(1,40)
 print(lis)
-ist noch nixht fertig !!!!!!!!!!!!!!!!
+
+#7. Print how many elements in the list
+'''
+look to question number 5 and 6
+'''
+
+#8. replace the second element in the list with 100
+
+lis=[10,11,12,13,14,15,16,17,18,19,20]
+lis[1] = '100'
+print(lis)
