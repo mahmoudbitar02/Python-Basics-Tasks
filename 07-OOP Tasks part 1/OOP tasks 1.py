f5b6b936f8d33a79b3969cf067323fdfa92ad9e8
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-26 18:18:47 +0100

    add OOP tasks part 1

---
+++ 07-OOP Tasks part 1/OOP tasks 1.py
@@ -0,0 +1,64 @@
+#1. Explain in few words what we mean by a class give and example
+'''
+class is that what the creater create it (main product) and for this we can create other product (attribut)
+'''
+#2. create a simple class names calculator
+'''
+class calculator:
+'''
+#3.Create a constructor that prints Welcome message
+class calculator:
+    def __init__(self):
+        print ('welcome')
+
+s=calculator()
+#4.Add 2 methods to the class sum & mull
+class calculator:
+    def __init__(self):
+        print ('welcome')
+    def sum():
+        pass
+
+    def mull():
+        pass
+s=calculator()
+
+#5.The sum method return the sum of 2 arguments xand y
+#6. The mull method return the multiplication of the arguments x and y
+class calculator:
+    def __init__(self):
+        print ('welcome')
+    def sum(x,y):
+        return
+    def mull(x,y):
+      return  
+s=calculator()
+
+#6. Take an object from the class
+class calculator:
+    def __init__(self):
+        print ('welcome')
+    def sum(self,x,y):
+        return
+    def mull(self,x,y):
+        return
+
+    
+s=calculator()
+
+
+#7. Call the sum method with 10 , 20
+#8. Call the mull method with 10 , 20
+class calculator:
+    def __init__(self):
+        print ('welcome')
+    def sum(self,x,y):
+        self.sum=x+y 
+        print(self.sum)
+        
+    def mull(self,x,y):
+        self.mull=x*y
+        print(self.mull)
+s=calculator()
+s.sum(10,20)
+s.mull(10,20)
