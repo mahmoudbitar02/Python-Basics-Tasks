fc104ff71316b1b3ebd9bef19e4b3f00003c9c89
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-26 19:19:59 +0100

    add Calc

--- 07-OOP Tasks part 1/OOP tasks part 2/OOp part 2.py
+++ 07-OOP Tasks part 1/OOP tasks part 2/OOp part 2.py
@@ -17,6 +17,7 @@ class SciCalc():
     def power(self,x,y):
         return(x**y)
 #5. Take an object from the class and call the 3 methodswith any numbers
+'''
 class SciCalc():
     def sum(self,x,y):
         return(x+y)
@@ -32,3 +33,33 @@ s.power(2,3)
 print(s.sum(2,4))
 print(s.mull(3,5))
 print(s.power(2,3))
+'''
+#6. Can we inherit from the class we created in the first task Calc
+'''
+yes we can
+'''
+#7. Inherit from the Calc class , now remove theunneeded code the the SciCalc after inheriting
+class calc:
+    def sum(self,x,y):
+        return(x+y)
+    def mull(self,x,y):
+        return(x*y)
+
+class SciCalc(calc):
+   
+    def power(self,x,y):
+        return(x**y)
+
+
+s=calc()
+s.sum(2,4)
+s.mull(3,5)
+print(s.sum(2,4))
+print(s.mull(3,4))
+s=SciCalc()
+s.power(2,3)
+print(s.power(2,3))
+
+
+
+
