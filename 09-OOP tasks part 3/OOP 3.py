94a09b4c76f5ec0e648e8c677300f96a697d6345
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-11-28 22:14:26 +0100

    add OOP3

--- 09-OOP tasks part 3/OOP 3.py
+++ 09-OOP tasks part 3/OOP 3.py
@@ -1,2 +1,28 @@
 #1.What do we mean with the term :
 #1. Base class
+'''
+A base class is a class, in an object-oriented programming language, from which other classes are derived.
+
+'''
+#2.Parent class
+'''
+Parent class is the class being inherited from, also called base class.
+'''
+#3.Sub class
+'''
+when we inhere from Class to another Class, the second Class should be the subclass
+'''
+#4.Main class
+'''
+if __name__ == "__main__" :
+with this class we can choss where should the code begen 
+'''
+#5.Derived class
+'''
+it is child class the class that inherits from another class, also called derived class
+'''
+#6.Instance
+'''
+instance of a class is also called an object. The call will comprise both data members and methods and will be accessed by an object of that class.
+'''
+#2.Explain in few words what we mean with the word polymorphism , give an example
